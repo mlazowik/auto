6f7ae784ac008376dafe54e4e3e5bc63fad98a8e
Michał Łazowik <mlazowik@gmail.com>
2017-03-21 02:33:34 +0100

    Add leftA

--- src/Auto.hs
+++ src/Auto.hs
@@ -20,7 +20,20 @@ symTransition c q a | not q && a == c = [True]
                     | otherwise = []
 
 symA :: (Eq a) => a -> Auto a Bool
-symA c = A {states=[True, False], initStates = [False], isAccepting = id, transition = symTransition c}
+symA c = A {states = [True, False], initStates = [False], isAccepting = id, transition = symTransition c}
+
+leftAccepting :: Auto a q -> Either q r -> Bool
+leftAccepting aut (Left q) = isAccepting aut q
+leftAccepting _ _ = False
+
+leftTransition :: Auto a q ->  Either q r -> a -> [Either q r]
+leftTransition aut (Left q) c = Left <$> transition aut q c
+leftTransition _ _ _ = []
 
 leftA :: Auto a q -> Auto a (Either q r)
-leftA aut =
+leftA aut = A {
+  states = Left <$> states aut
+, initStates = Left <$> initStates aut
+, isAccepting = leftAccepting aut
+, transition = leftTransition aut
+}
