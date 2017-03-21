ff2dfa713a2a9d0acd606644f05f7558d01c2630
Michał Łazowik <mlazowik@gmail.com>
2017-03-21 02:47:20 +0100

    Add sumA

--- src/Auto.hs
+++ src/Auto.hs
@@ -37,3 +37,19 @@ leftA aut = A {
 , isAccepting = leftAccepting aut
 , transition = leftTransition aut
 }
+
+sumAccepting :: Auto a q1 -> Auto a q2 -> Either q1 q2 -> Bool
+sumAccepting aut1 _ (Left q) = isAccepting aut1 q
+sumAccepting _ aut2 (Right q) = isAccepting aut2 q
+
+sumTransition :: Auto a q1 -> Auto a q2 -> Either q1 q2 -> a -> [Either q1 q2]
+sumTransition aut1 _ (Left q) = Left <$> transition aut1 q
+sumTransition _ aut2 (Right q) = Right <$> transition aut2 q
+
+sumA :: Auto a q1 -> Auto a q2 -> Auto a (Either q1 q2)
+sumA aut1 aut2 = A {
+  states = (Left <$> states aut1) ++ (Right <$> states aut2)
+, initStates = (Left <$> initStates aut1) ++ (Right <$> initStates aut2)
+, isAccepting = sumAccepting aut1 aut2
+, transition = sumTransition aut1 aut2
+}
