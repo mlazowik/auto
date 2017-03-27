137b40174e6b24c33f2f252866b6f910faca87ec
Michał Łazowik <mlazowik@gmail.com>
2017-03-27 13:33:45 +0200

    Refactor concatting transitions

--- src/Auto.hs
+++ src/Auto.hs
@@ -74,14 +74,17 @@ nextAccepting :: Auto a q -> q -> a -> Bool
 nextAccepting aut q c = any (isAccepting aut) (transition aut q c)
 
 leftToRightTransition :: Auto a q1 -> Auto a q2 -> q1 -> a -> [Either q1 q2]
-leftToRightTransition aut1 aut2 q c = (Left <$> transition aut1 q c) ++ (if nextAccepting aut1 q c then Right <$> initStates aut2 else [])
+leftToRightTransition aut1 aut2 q c = if nextAccepting aut1 q c then Right <$> initStates aut2 else []
+
+concatTransition :: (t1 -> t -> [a]) -> (t1 -> t -> [a]) -> t1 -> t -> [a]
+concatTransition t1 t2 q c = t1 q c ++ t2 q c
 
 thenA :: Auto a q1 -> Auto a q2 -> Auto a (Either q1 q2)
 thenA aut1 aut2 = A{
   states = (Left <$> states aut1) ++ (Right <$> states aut2)
 , initStates = (Left <$> initStates aut1) ++ (if hasAcceptingInitState aut1 then Right <$> initStates aut2 else [])
 , isAccepting = either (if hasAcceptingInitState aut2 then isAccepting aut1 else const False) (isAccepting aut2)
-, transition = either (leftToRightTransition aut1 aut2) (rightTransition aut2)
+, transition = either (concatTransition (leftTransition aut1) (leftToRightTransition aut1 aut2)) (rightTransition aut2)
 }
 
 fromLists :: (Eq q, Eq a) => [q] -> [q] -> [q] -> [(q,a,[q])] -> Auto a q
