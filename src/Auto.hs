2dbc8f761adc1cb3e62004646cca09ac6fd73bce
Michał Łazowik <mlazowik@gmail.com>
2017-03-27 03:04:13 +0200

    Get rid of rightStatesOrEmpty

--- src/Auto.hs
+++ src/Auto.hs
@@ -62,19 +62,16 @@ sumA aut1 aut2 = A {
 hasAcceptingInitState :: Auto a q -> Bool
 hasAcceptingInitState aut = any (isAccepting aut) (initStates aut)
 
-rightStatesOrEmpty :: Bool -> [b] -> [Either a b]
-rightStatesOrEmpty predicate s = if predicate then Right <$> s else []
-
 nextAccepting :: Auto a q -> q -> a -> Bool
 nextAccepting aut q c = any (isAccepting aut) (transition aut q c)
 
 leftToRightTransition :: Auto a q1 -> Auto a q2 -> q1 -> a -> [Either q1 q2]
-leftToRightTransition aut1 aut2 q c = (Left <$> transition aut1 q c) ++ rightStatesOrEmpty (nextAccepting aut1 q c) (initStates aut2)
+leftToRightTransition aut1 aut2 q c = (Left <$> transition aut1 q c) ++ (if nextAccepting aut1 q c then Right <$> initStates aut2 else [])
 
 thenA :: Auto a q1 -> Auto a q2 -> Auto a (Either q1 q2)
 thenA aut1 aut2 = A{
   states = (Left <$> states aut1) ++ (Right <$> states aut2)
-, initStates = (Left <$> initStates aut1) ++ rightStatesOrEmpty (hasAcceptingInitState aut1) (initStates aut2)
+, initStates = (Left <$> initStates aut1) ++ (if hasAcceptingInitState aut1 then Right <$> initStates aut2 else [])
 , isAccepting = either (if hasAcceptingInitState aut2 then isAccepting aut1 else const False) (isAccepting aut2)
 , transition = either (leftToRightTransition aut1 aut2) (rightTransition aut2)
 }
