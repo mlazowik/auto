55d776b690f986ef7969308213d4802269ce4d2b
Michał Łazowik <mlazowik@gmail.com>
2017-03-27 15:15:24 +0200

    Refactor thenA

--- src/Auto.hs
+++ src/Auto.hs
@@ -14,10 +14,10 @@ module Auto
 import qualified Data.List as List
 
 data Auto a q = A
-  { states :: [q]
-  , initStates :: [q]
+  { states      :: [q]
+  , initStates  :: [q]
   , isAccepting :: q -> Bool
-  , transition :: q -> a -> [q]
+  , transition  :: q -> a -> [q]
   }
 
 instance (Show a, Enum a, Bounded a, Show q) =>
@@ -66,40 +66,20 @@ sumA aut1 aut2 =
   , transition = either ((fmap Left .) . transition aut1) ((fmap Right .) . transition aut2)
   }
 
-hasAcceptingInitState :: Auto a q -> Bool
-hasAcceptingInitState aut = any (isAccepting aut) (initStates aut)
-
-nextAccepting :: Auto a q -> q -> a -> Bool
-nextAccepting aut q c = any (isAccepting aut) (transition aut q c)
-
-leftToRightTransition :: Auto a q1 -> Auto a q2 -> q1 -> a -> [Either q1 q2]
-leftToRightTransition aut1 aut2 q c =
-  if nextAccepting aut1 q c
-    then Right <$> initStates aut2
-    else []
-
-concatTransition :: (q -> a -> [q1]) -> (q -> a -> [q1]) -> q -> a -> [q1]
-concatTransition t1 t2 q c = t1 q c ++ t2 q c
+addLeftToRight :: Auto a q1 -> Auto a q2 -> [q1] -> [Either q1 q2]
+addLeftToRight aut1 aut2 toStates =
+  (Left <$> toStates) ++
+  (if any (isAccepting aut1) toStates
+     then Right <$> initStates aut2
+     else [])
 
 thenA :: Auto a q1 -> Auto a q2 -> Auto a (Either q1 q2)
 thenA aut1 aut2 =
   A
   { states = (Left <$> states aut1) ++ (Right <$> states aut2)
-  , initStates =
-      (Left <$> initStates aut1) ++
-      (if hasAcceptingInitState aut1
-         then Right <$> initStates aut2
-         else [])
-  , isAccepting =
-      either
-        (if hasAcceptingInitState aut2
-           then isAccepting aut1
-           else const False)
-        (isAccepting aut2)
-  , transition =
-      either
-        (concatTransition ((fmap Left .) . transition aut1) (leftToRightTransition aut1 aut2))
-        ((fmap Right .) . transition aut2)
+  , initStates = addLeftToRight aut1 aut2 (initStates aut1)
+  , isAccepting = either (const False) (isAccepting aut2)
+  , transition = either ((addLeftToRight aut1 aut2 .) . transition aut1) ((fmap Right .) . transition aut2)
   }
 
 fromLists
