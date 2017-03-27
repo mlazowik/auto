ee4de1f757a7fb55b2ba9e9175ab238edc264430
Michał Łazowik <mlazowik@gmail.com>
2017-03-27 02:20:29 +0200

    Refactor out left to right transition

--- src/Auto.hs
+++ src/Auto.hs
@@ -40,7 +40,7 @@ symA c = A {states = [True, False], initStates = [False], isAccepting = id, tran
 leftTransition :: Auto a q -> q -> a -> [Either q r]
 leftTransition aut q c = Left <$> transition aut q c
 
-rightTransition :: Auto a r -> r -> a -> [Either q r]
+rightTransition :: Auto a q -> q -> a -> [Either l q]
 rightTransition aut q c = Right <$> transition aut q c
 
 leftA :: Auto a q -> Auto a (Either q r)
@@ -65,12 +65,18 @@ hasAcceptingInitState aut = any (isAccepting aut) (initStates aut)
 rightStatesOrEmpty :: Bool -> [b] -> [Either a b]
 rightStatesOrEmpty predicate s = if predicate then Right <$> s else []
 
+nextAccepting :: Auto a q -> q -> a -> Bool
+nextAccepting aut q c = any (isAccepting aut) (transition aut q c)
+
+leftToRightTransition :: Auto a q1 -> Auto a q2 -> q1 -> a -> [Either q1 q2]
+leftToRightTransition aut1 aut2 q c = (Left <$> transition aut1 q c) ++ rightStatesOrEmpty (nextAccepting aut1 q c) (initStates aut2)
+
 thenA :: Auto a q1 -> Auto a q2 -> Auto a (Either q1 q2)
 thenA aut1 aut2 = A{
   states = (Left <$> states aut1) ++ (Right <$> states aut2)
 , initStates = (Left <$> initStates aut1) ++ rightStatesOrEmpty (hasAcceptingInitState aut1) (initStates aut2)
 , isAccepting = either (if hasAcceptingInitState aut2 then isAccepting aut1 else const False) (isAccepting aut2)
-, transition = either (\ q c -> (Left <$> transition aut1 q c) ++ rightStatesOrEmpty (any (isAccepting aut1) (transition aut1 q c)) (initStates aut2)) (rightTransition aut2)
+, transition = either (leftToRightTransition aut1 aut2) (rightTransition aut2)
 }
 
 fromLists :: (Eq q, Eq a) => [q] -> [q] -> [q] -> [(q,a,[q])] -> Auto a q
