cd0ef1e16274e69d1bde2b6be823b668b63ce533
Michał Łazowik <mlazowik@gmail.com>
2017-03-28 15:14:51 +0200

    Parse everything but transitions v2

--- app/RunAuto.hs
+++ app/RunAuto.hs
@@ -14,17 +14,38 @@ main = do
   else
     die "Usage: RunAuto filename"
 
+range :: (Enum t, Num t) => t -> [t]
 range n = [1..n]
 
-parseStates input = range <$> (readMaybe input :: Maybe Int)
+parseMaxState :: String -> Maybe Int
+parseMaxState input = do
+  maxState <- readMaybe input
+  if maxState < 1 then Nothing else return maxState
 
-parseStatesList input = readMaybe input :: Maybe [Int]
+parseStatesList :: Int -> String -> Maybe [Int]
+parseStatesList maxState input = do
+  statesList <- readMaybe input
+  if all (\x -> 1 <= x && x <= maxState) statesList then return statesList else Nothing
 
-parseTransitions = id
+parseTransitions :: Int -> [String] -> Maybe [String]
+parseTransitions maxState = Just
 
-parse (states:initStates:ackStates:rest) = (parseStates states, parseStatesList initStates, parseStatesList ackStates,
- parseTransitions $ tail $ reverse rest, last rest)
+parseWord :: [String] -> Maybe String
+parseWord [] = Nothing
+parseWord lst = Just (last lst)
 
+parse :: [String] -> Maybe ([Int], [Int], [Int], [String], String)
+parse (s:is:as:rest) = do
+  maxState <- parseMaxState s
+  let states = [1..maxState]
+  initStates <- parseStatesList maxState is
+  ackStates <- parseStatesList maxState as
+  transitions <- parseTransitions maxState $ init rest
+  word <- parseWord rest
+  return (states, initStates, ackStates, transitions, word)
+parse _ = Nothing
+
+handle :: String -> String
 handle input = show $ parse (getLines input)
   where
     getLines = (filter (not . null) .) lines
