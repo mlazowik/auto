5d0be78594ce856e40ee350885c444a0db6e6258
Michał Łazowik <mlazowik@gmail.com>
2017-03-28 13:37:15 +0200

    Parse everything but transitions

--- app/RunAuto.hs
+++ app/RunAuto.hs
@@ -1,5 +1,6 @@
 import System.Environment( getArgs )
 import System.Exit( die )
+import Text.Read( readMaybe )
 
 import qualified Auto
 
@@ -8,9 +9,22 @@ main = do
   args <- getArgs
   if length args == 1 then
     do
-      content <- readFile $ head args
-      putStrLn $ handle content
+      input <- readFile $ head args
+      putStrLn $ handle input
   else
     die "Usage: RunAuto filename"
 
-handle = id
+range n = [1..n]
+
+parseStates input = range <$> (readMaybe input :: Maybe Int)
+
+parseStatesList input = readMaybe input :: Maybe [Int]
+
+parseTransitions = id
+
+parse (states:initStates:ackStates:rest) = (parseStates states, parseStatesList initStates, parseStatesList ackStates,
+ parseTransitions $ tail $ reverse rest, last rest)
+
+handle input = show $ parse (getLines input)
+  where
+    getLines = (filter (not . null) .) lines
